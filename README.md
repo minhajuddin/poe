368b2f0999de78a4d121d555d817bf03a92be179
Khaja Minhajuddin <minhajuddin@cosmicvent.com>
2010-11-08 11:24:49 +0530

    added a config file and added the code which was deleted

--- README.md
+++ README.md
@@ -6,6 +6,14 @@ Utility to configure a cijoe for a repository
   - it copies `.build-failed` `.build-worked` files from your `~/.poe/` directory to
     your `.git/hooks` directory
 
+TODO
+====
+  1. Create a directory called `~/.poe` on installation and copy the `hooks`
+     directory to `~/.poe` also copy the config file `poe.yml` to 
+  2. Change the script to check the config file for the default runner and the
+     *to* email id
+  3. Finally, make it wireup cijoe so that it starts as a deamon on login
+
 Copyright
 =========
 
--- lib/poe.rb
+++ lib/poe.rb
@@ -1,5 +1,7 @@
 class Poe
 
+  @@hooks = ['build-failed', 'build-worked', 'common.rb']
+
   def initialize(dir)
     @dir = dir
   end
@@ -9,16 +11,35 @@ class Poe
       display_errors
       return
     end
-
+    setup
     puts "drop kick => '#@dir'"
   end
 
   def setup
     #copy the hooks
+    copy_hooks
     #setup the default runner
-    
+    setup_runner
   end
 
+
+  def copy_hooks
+    src_dir = File.join(home_dir, '.poe', 'hooks')
+    tgt_dir = File.join(@dir, '.git', 'hooks')
+
+    @@hooks.each do |h|
+      FileUtils.cp File.join(src_dir, h), tgt_dir
+    end
+
+  end
+
+  def setup_runner
+    `git config --add cijoe.runner bundle exec rspec spec`
+  end
+
+  def home_dir
+    File.expand_path('~')
+  end
 
   def valid?
     errors << "'#{@dir}' does not exist" unless File.exists?(@dir)
